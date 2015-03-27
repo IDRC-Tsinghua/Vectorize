e511e809daf5019769c86862b1cac5c7701e458a
dongchao <dongchao@xiaomi.com>
2015-03-27 15:57:33 +0900

    vectorize frist version

--- src/data_filter.py
+++ src/data_filter.py
@@ -1,6 +1,21 @@
 # -*- coding=utf-8 -*-
 import config
 import json
+from gensim import corpora, models, similarities
+
+"""
+Return Function:
+----------------
+
+get_text_from_file
+
+write_to_file
+
+get_stopwords
+
+character_to_vector
+
+"""
 
 def get_text_from_file(path):
     """ get the pure chinese line from file
@@ -13,6 +28,7 @@ def get_text_from_file(path):
     -------
     texts: format: [["aa bb cc"],["a  sd  d"]..  ]
            all the character here is Chinese
+           type: str list list
     """
     texts = []
     with open(path) as file_ob:
@@ -23,7 +39,7 @@ def get_text_from_file(path):
             words = word_with_property.split(" ")
             # from each line we get a word_list at last
             word_list = [word.split("/")[0] for word in words]
-            texts.append(' '.join(word_list))
+            texts.append(word_list)
 
     return texts
 
@@ -49,13 +65,69 @@ def write_to_file(data, file_name):
     file_ob.close()
     return
 
+def get_stopwords():
+    """ get the stopwords list from file
+
+    Parameters:
+    -----------
+    None
+
+    Return:
+    -------
+    stopwords: the stopwords list
+               type: str list
+
+    """
+    stopwords_list = []
+    with open(config.stopwords_path) as file_ob:
+        for line in file_ob:
+            stopwords_list.append(line)
+        file_ob.close()
+
+    return stopwords_list
+
+
+
+def character_to_vector(texts):
+    """ change all word in the texts to numeric vector
+
+    Parameters:
+    -----------
+    texts: two dimension list
+           format: str list list
+
+    Returns:
+    --------
+    dictionary:
+
+    """
+
+    dictionary = corpora.Dictionary(texts)
+    dictionary.save("../data/words.dict")
+    token2id = dictionary.token2id
+
+    texts_vec = dictionary.doc2bow(texts[0])
+    print texts_vec
+    return dictionary
+
 def main():
     """ the main function
 
     """
+
+    print "prepare text..."
     texts = get_text_from_file(config.frozen_data_path)
-    write_file_path = "../data/chinese_data.txt"
-    write_to_file(texts, write_file_path)
+
+    print "prepare stopwords..."
+    stopwords = get_stopwords()
+
+    print "filter stopwords from texts..."
+    texts = [ [word for word in text if word not in stopwords] for text in texts ]
+
+    print "get directory by corpora..."
+    dictionary = character_to_vector(texts)
+
+
 
     return
 
